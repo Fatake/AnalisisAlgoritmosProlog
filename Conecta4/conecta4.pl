d7f8c88c148c176cb1fe9f1ae622837a6dbac67c
Fatake Raicos <pace_lozano@hotmail.com>
2019-11-11 09:44:13 -0600

    Proyectos

--- Conecta4/conecta4.pl
+++ Conecta4/conecta4.pl
@@ -7,33 +7,33 @@ tableroInicial(board([['-','-','-','-','-','-'],
                       ['-','-','-','-','-','-'],
                       ['-','-','-','-','-','-']])).
 
-%<--------------------------------------------- Muestra Tablero ----------------------------------------------->%
+%<--------------------------------------------- Imprime Tablero ----------------------------------------------->%
 
-%show(X) shows board X
-show(board(X)):- write('  A B C D E F G'), nl,
-                 iShow(X,6).
+% muestra(Tablero) Muestra el tablero
+muestra(board(X)):- write('  A B C D E F G'), nl, imuestra(X,6).
 
-%show(X,N) shows lines [N .. 1] of board X
-iShow(_,0).
-iShow(X,N):- showLine(X,N,X2),
-             Ns is N-1,
-             iShow(X2,Ns).
+% imuestra(X,N) muestras lineas de [N .. 1] en el tablero X
+imuestra(_,0).
+imuestra(X,N):- muestraLinea(X,N,X2),
+                Ns is N-1,
+                imuestra(X2,Ns).
 
-%showLine(X,N,X2) writes N and shows first line of board X (first element of every column). X2 is X without the shown line.
-showLine(X,N,X2):- write(N), write(' '),
-                   iShowLine(X,X2), nl.
+% muestraLinea(X,N,X2) escribe N y muestra la primera línea del tablero X (primer elemento de cada columna)
+% X2 es X sin la línea de mostrar.
+muestraLinea(X,N,X2):- write(N), write(' '),
+                   imuestraLinea(X,X2), nl.
 
-%iShowLine(X,X2) writes first element of every column. X2 is X without the shown line.
-iShowLine([],_).
-iShowLine([[X|X2]|XS],[X2|XS2]):- write(X), write(' '),
-                                  iShowLine(XS,XS2).
+%imuestraLinea(X,X2) writes first element of every column. X2 is X without the muestran line.
+imuestraLinea([],_).
+imuestraLinea([[X|X2]|XS],[X2|XS2]):- write(X), write(' '),
+                                  imuestraLinea(XS,XS2).
 
 %<----------------------------------------------Iniciar Juego-------------------------------------------------->%
 %
 %   Inicializa El Juego
 %
 iniciaJuegoC4:- tableroInicial(X),
-                show(X),
+                muestra(X),
                 sigTiro('X',X), !.
 
 %sigTiro(J,X) J es el jugador que necesita tirar ('O' o 'X') y X es el tablero
@@ -41,16 +41,16 @@ iniciaJuegoC4:- tableroInicial(X),
 
 sigTiro('X',X):- wins('O',X), write('Perdiste u.u').
 sigTiro('O',X):- wins('X',X), write('Ganaste n.n').
-sigTiro(_,X):- full(X), write('Empate -.-').
+sigTiro(_,X)  :- full(X),     write('Empate -.-').
 
 sigTiro('X',X):- repeat, %repeats in case a column is full
                   readColumn(C),
                   play('X',C,X,X2), !,
-                  show(X2),
+                  muestra(X2),
                   sigTiro('O',X2).
 
 sigTiro('O',X):- machine('O','X',X,X2),
-                  show(X2),
+                  muestra(X2),
                   sigTiro('X',X2).
 
 %play(X,P,T,T2) is satisfied if T2 is the board T after player X moves in column P
@@ -61,21 +61,24 @@ play(X,P,board(T),board(T2)):- append(I,[C|F],T),
 
 %playColumn(X,C,C2) is satisfied if column C2 is column C after player X plays there
 playColumn(X,['-'],[X]):- !. % last spot in column
-playColumn(X,['-',A|AS],[X,A|AS]):- A \== ('-'), !. % play above someone's piece
+playColumn(X,['-',A|AS],[X,A|AS]):- A \== ('-'), !. % jugar por encima de la pieza de alguien
 playColumn(X,['-'|AS],['-'|AS2]):- playColumn(X,AS,AS2). % descend column
 
-%wins(X,T) is satisfied if player X has won in board T
-%check if there's a column in T with 4 connected pieces of player X
-wins(X,board(T)):- append(_, [C|_], T), % check if there's a column...
+%<-------------------------------------------Ganar------------------------------------------------------------>%
+
+% comprobar si hay una columna en T con 4 piezas conectadas de jugador X
+wins(X,board(T)):- append(_, [C|_], T), % Checa si hay una columna
                    append(_,[X,X,X,X|_],C). % ...which has 4 connected pieces of player X
-%check if there's a row in T with 4 connected pieces of player X
+
+% comprobar si hay una fila en T con 4 piezas conectadas de jugador X
 wins(X,board(T)):- append(_,[C1,C2,C3,C4|_],T), % check if 4 connected columns exists in board...
                    append(I1,[X|_],C1), %...such that all of them contain a piece of player X...
                    append(I2,[X|_],C2),
                    append(I3,[X|_],C3),
                    append(I4,[X|_],C4),
                    length(I1,M), length(I2,M), length(I3,M), length(I4,M). %...and every piece is in the same height
-%check if there's a diagonal (type \) in T with 4 connected pieces of player X
+
+% comprueba si hay una diagonal (tipo \) en T con 4 piezas conectadas de jugador X
 wins(X,board(T)):- append(_,[C1,C2,C3,C4|_],T), % check if 4 connected columns exists in board...
                    append(I1,[X|_],C1), %...such that all of them contain a piece of player X...
                    append(I2,[X|_],C2),
@@ -97,7 +100,7 @@ full(board(T)):- \+ (append(_,[C|_],T),
                  append(_,['-'|_],C)).
 
 %<-------------------------------------------------------Registra el tiro-------------------------------------->%
-%reads a column
+%leeColumna
 readColumn(C):- nl, write('Tiro(ColFil): '),
                 repeat,
                 get_char(L),
@@ -125,32 +128,32 @@ col(6).
 %machine(R,O,T,T2) Let R be the machine piece, O the opponent's piece and T the board game. Then T2 is board T after the machine movement
 % win if possible
 machine(R,_,T,T2):- iMachine(R,T,C,T2),
-                    nl, write('machine: '),
+                    nl, write('computadora: '),
                     associateChar(L,C),
                     write(L),
                     nl,!.
 % otherwise, if machine can't win within a move, play a move that doesn't allow opponent O to win and that would allow us to obtain a connected 4
 machine(R,O,T,T2):- findall((Col,TA), (col(Col), play(R,Col,T,TA),\+ iMachine(O,TA,_,_), goodMove(R,Col,T)), [(C,T2)|_]),
-                    nl, write('machine: '),
+                    nl, write('computadora: '),
                     associateChar(L,C),
                     write(L),
                     nl,!.
 % otherwise play a move that doesn't allow opponent O to win
 machine(R,O,T,T2):- findall((Col,TA), (col(Col), play(R,Col,T,TA),\+ iMachine(O,TA,_,_)), [(C,T2)|_]),
-                    nl, write('machine: '),
+                    nl, write('computadora: '),
                     associateChar(L,C),
                     write(L), nl,
                     write('-'),!.
 % otherwise play a move intercepting one of the future winning options of opponent O
 machine(R,O,T,T2):- iMachine(O,T,C,_),
                     play(R,C,T,T2),
-                    nl, write('machine: '),
+                    nl, write('computadora: '),
                     associateChar(L,C),
                     write(L), nl.
 % otherwise play wherever
 machine(R,_,T,T2):- col(C),
                     play(R,C,T,T2),
-                    nl, write('machine: '),
+                    nl, write('computadora: '),
                     associateChar(L,C),
                     write(L), nl.
                                   
